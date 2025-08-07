ff5eea218996986fde5c017399f0d50bf3e602ca
getshubhamsingh <147321136+getshubhamsingh@users.noreply.github.com>
2025-08-07 20:01:01 +0530

    Delete WEEK 7/WEEK 6 directory

--- WEEK 7/WEEK 6/2 ReactJS-HOL/studentapp/src/Components/About.js
+++
@@ -1,9 +0,0 @@
-// src/Components/About.js
-import React from 'react';
-
-function About() {
-  return <h1>Welcome to the About page of the Student Management Portal</h1>;
-}
-
-export default About;
-
--- WEEK 7/WEEK 6/2 ReactJS-HOL/studentapp/src/Components/Home.js
+++
@@ -1,8 +0,0 @@
-// src/Components/Home.js
-import React from 'react';
-
-function Home() {
-  return <h1>Welcome to the Home page of Student Management Portal</h1>;
-}
-
-export default Home;
