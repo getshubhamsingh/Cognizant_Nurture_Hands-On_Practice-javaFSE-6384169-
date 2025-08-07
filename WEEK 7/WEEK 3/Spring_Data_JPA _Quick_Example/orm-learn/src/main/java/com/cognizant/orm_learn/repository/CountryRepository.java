864bf571ed8322e6b3a40e46c129c4a60ac8fb51
getshubhamsingh <147321136+getshubhamsingh@users.noreply.github.com>
2025-08-07 20:00:48 +0530

    Delete WEEK 7/WEEK 3 directory

--- WEEK 7/WEEK 3/Spring_Data_JPA _Quick_Example/orm-learn/src/main/java/com/cognizant/orm_learn/repository/CountryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cognizant.orm_learn.repository;
-
-import com.cognizant.orm_learn.model.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountryRepository extends JpaRepository<Country, String> {
-
-}
